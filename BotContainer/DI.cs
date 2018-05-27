0941bae3542bd1b5a5e8c23ac01635603546d14b
jdk <zhe.dan28@gmail.com>
2018-05-27 11:17:59 +0500

    Initial commit

---
+++ BotContainer/DI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using VKApiCore;
+
+namespace BotContainer
+{
+    public class DI
+    {
+        public static IKernel container { get; }
+
+        static DI()
+        {
+            container = new StandardKernel();
+
+            container.Bind(syntax => syntax
+                .FromThisAssembly()
+                .SelectAllClasses()
+                .BindAllInterfaces()
+                .Configure(onSyntax => onSyntax.InSingletonScope())
+            );
+        }
+    }
+}
---
+++ BotContainer/DI_should.cs
@@ -0,0 +1,22 @@
+﻿using Ninject;
+using NUnit.Framework;
+using VKApiCore;
+
+namespace BotContainer
+{
+    [TestFixture]
+    class DI_should
+    {
+        private static readonly IKernel Container = DI.container;
+
+        [Test]
+        public void TestContainer()
+        {
+            var instance1 = Container.Get<IVkService>();
+            var instance2 = Container.Get<IVkService>();
+            Assert.AreNotEqual(null, instance1);
+            Assert.AreEqual(typeof(VkService), instance1);
+            Assert.AreEqual(instance1, instance2);
+        }
+    }
+}
